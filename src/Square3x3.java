ea6262ed332e5737ff859babf113c4c1daaa5465
Arel Sharon <arels@assafmedia.co.il>
2021-11-28 22:40:55 +0200

    Added API and documantation, tested using testers.

--- src/Square3x3.java
+++ src/Square3x3.java
@@ -1,7 +1,14 @@
+/**
+ *
+ */
 public class Square3x3 {
     final int DEFAULT_CELL_VALUE = -1;
     private int _cellArr[][];
 
+    /**
+     * Default constructor, constructs and initializes the 2-dimensional
+     * array of the size 3X3, with the values of –1 in each cell.
+     */
     public Square3x3(){
         _cellArr = new int[3][3];
         for (int i = 0 ; i <3 ; i++ )
@@ -9,48 +16,92 @@ public class Square3x3 {
                 _cellArr[i][j] = DEFAULT_CELL_VALUE;
 
     }
+
+    /**
+     * Constructs a 2-dimensional array of the size 3X3, whose values are taken from the given
+     * array. If the given array’s size is bigger than 3X3, only the first 3X3 cells are taken. If the
+     * given array is smaller, the rest of the cells are initialized to –1.x
+     * @param array 2 dimensional array from
+     */
     public Square3x3(int[][]array){
+
         _cellArr = new int[3][3];
+
         int dereferenceRange = array.length;
         int innerDereferenceRange = 0;
 
         for (int i = 0 ; i <3 ; i++ ){
+            // Retrieves current iteration's second index length to combat unequal length of seconds index
             if(i<dereferenceRange)
                 innerDereferenceRange = array[i].length;
             for(int j = 0 ; j< 3 ; j++){
+                //Default value is inserted first, will be overridden if available
                 _cellArr[i][j] = DEFAULT_CELL_VALUE;
 
-                //Array[i] is guaranteed to be defined within this scope
+                //Make sure iterator is within the dereference range of the second index
                 if(j<innerDereferenceRange){
-                    //Give array at [i][j] is defined
-                    _cellArr[i][j] = array[i][j];
+                    //Corresponding value exists! override default cell value placed before, after checking the value is indeed a 1-9 digit
+                    if(array[i][j]>0 && array[i][j]<=9)
+                        _cellArr[i][j] = array[i][j];
                 }
-
             }
         }
     }
+
+    /**
+     * Copy constructor. Constructs a 2-dimensional array of the size 3X3, whose values are
+     * taken from the array of the given Square3x3 object.
+     * @param other Other 3x3 Square
+     */
     public Square3x3(Square3x3 other){
         _cellArr = new int[3][3];
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3 ; j++)
                 _cellArr[i][j] = other.getCell(i,j);
     }
+
+    /**
+     * Returns the value in the (row, col) cell. If the row and/or col are out of the
+     * array bounds, returns –1. Legal values for row/col are 0,1,2.
+     * @param row row number (0-2)
+     * @param col col number (0-2)
+     * @return value of cell at the [row][col] index, -1 on invalid params
+     */
     public int getCell(int row,int col){
+
+        //Check if dereference given is valid, returns -1 if not
         if(!isValidRowCol(row,col)){
             return -1;
         }
         return _cellArr[row][col];
     }
+
+    /**
+     * Sets the cell (row, col) in the array to the given value. If the row and/or col
+     * are out of the array bounds – does nothing. Legal values for row/col are
+     * 0,1,2.
+     * @param row row number (0-2)
+     * @param col col number (0-2)
+     * @param value value to set to corresponding row,col
+     */
     public void setXY(int row,int col,int value){
+        //Checks if row&col are valid before dereferences array
         if(isValidRowCol(row,col)){
             _cellArr[row][col] = value;
         }
     }
+
+    /**
+     * Check if all digits(1-9) are represented in the grid
+     * @return true if digits are there
+     */
     public boolean allThere(){
-        boolean isdigitPresent[] = new boolean[10]; //Array[0] is not user as there is no 0 in suduko
+        boolean isdigitPresent[] = new boolean[10]; //Array[0] is not used as there is no 0 in sudoku
         for (int i = 0; i < 3 ; i++) {
             for (int j = 0; j < 3 ; j++) {
                 int currentCellValue = getCell(i,j);
+
+                //This is mostly not needed as values are checked on contruction of the grid, but I chose to check anyways
                 if(currentCellValue <= isdigitPresent.length && currentCellValue > 0){
                     //Cell Value is 1-9
                     isdigitPresent[currentCellValue] =true;
@@ -59,32 +110,33 @@ public class Square3x3 {
         }
         //Make sure not to use the first cell as it is not in use
         for (int i = 1; i < isdigitPresent.length; i++) {
-            if(!isdigitPresent[i]) return false;
+            if(!isdigitPresent[i]) return false; //If *any* digit between 1-9 is not present return false
         }
         return true;
     }
+    /**
+     * Marks the digits that are present in given row at the digit's index in the given array, leaves rest of cells unchanged
+     * @param row row in grid to check (0-2)
+     * @param values boolean array to mark corresponding values as true
+     */
     public void whosThereRow (int row, boolean[] values ){
-        if(isValidOffset(row)) {
-            int currentCellValue;
-            for (int i = 0; i < 3; i++) {
-                currentCellValue = getCell(row,i);
-                if(currentCellValue < values.length && currentCellValue > 0){
-                    values[currentCellValue] = true;
-                }
-            }
-        }
+        _whosThereElement(row,values,"row");
     }
+
+    /**
+     * Marks the digits that are present in given col at the digit's index in the given array, leaves rest of cells unchanged
+     * @param col row in grid to check (0-2)
+     * @param values boolean array to mark corresponding values as true
+     */
     public void whosThereCol (int col, boolean[] values ){
-        if(isValidOffset(col)) {
-            int currentCellValue;
-            for (int i = 0; i < 3; i++) {
-                currentCellValue = getCell(i,col);
-                if(currentCellValue < values.length && currentCellValue > 0){
-                    values[currentCellValue] = true;
-                }
-            }
-        }
+        _whosThereElement(col,values,"col");
     }
+
+    /**
+     * represents the grid as a string
+     * @return string representation of the grid with tabs between columns and newlines between rows
+     *
+     */
     public String toString(){
         String output = "";
         for (int i = 0; i < 3; i++) {
@@ -99,10 +151,53 @@ public class Square3x3 {
         }
         return output;
     }
+
+    /**
+     * checks if row&col given are within bounds
+     * @param row row index
+     * @param col col index
+     * @return true if row&col given are valid
+     */
     private boolean isValidRowCol(int row, int col){
         return isValidOffset(row)&&isValidOffset(col);
     }
+
+    /**
+     * Check if offset to grid representation is valid
+     * @param offset index in grid
+     * @return true if offset is valid (0-2)
+     */
     private boolean isValidOffset(int offset){
         return !(offset > 2);
+    }
+
+    /**
+     * Marks the digits that are present in given row/col at the digit's index in the given array, leaves rest of cells unchanged
+     * @param elementIndex element in grid to check (0-2)
+     * @param values boolean array to mark corresponding values as true
+     * @param identifier type of element, "row" for row, any other string with represent a col index
+     */
+    private void _whosThereElement (int elementIndex, boolean[] values , String identifier ){
+        if(isValidOffset(elementIndex)) {
+
+            int currentCellValue;
+
+            for (int i = 0; i < 3; i++) {
+                if(identifier.equals("row")){
+                    //Element is row, fetch it
+                    currentCellValue = getCell(elementIndex,i);
+                }else{
+                    //Element is col, fetch it
+                    currentCellValue = getCell(i,elementIndex);
+                }
+                /*
+                    Because values can be any length, we must check that the digit is within the bounds of it
+                    Also we make sure that the cell value is not the default (-1) <=9 is implicit from constructor limitations
+                */
+                if(currentCellValue < values.length && currentCellValue > 0){
+                    values[currentCellValue] = true; //Mark digit as present
+                }
+            }
+        }
     }
 }
--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,5 +1,12 @@
+/**
+ *
+ */
 public class Sudoku {
     private Square3x3[][] _squareCluster;
+
+    /**
+     * Constructs a 3x3 board of 3x3 grid all init to the default value for 3x3 Square
+     */
     public Sudoku(){
         _squareCluster = new Square3x3[3][3];
         for (int i = 0; i < 3; i++) {
@@ -8,6 +15,11 @@ public class Sudoku {
             }
         }
     }
+
+    /**
+     * Constructs a board from a 2 dimensional array of 3x3 Squares
+     * @param square3x3Array array to init from
+     */
     public Sudoku(Square3x3[][] square3x3Array){
         _squareCluster = new Square3x3[3][3];
         for (int i = 0; i < 3; i++) {
@@ -16,51 +28,102 @@ public class Sudoku {
             }
         }
     }
-    public Boolean isValid(){
+
+    /**
+     * Check the entire board for validity
+     * @return true if board is a valid finished suduko board
+     */
+    public boolean isValid(){
+        /*
+            for a board to be valid 2 things need to happen:
+            (1) all the 3x3 clusters must have all 9 digits exactly once
+            (2) each row and columns must have all 9 digits exactly once
+         */
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3 ; j++) {
+                //Check every single 3x3 cluster for all the digits (1)
                 if(!_squareCluster[i][j].allThere()){
                     //if one square isn't valid the whole board is not valid
                     return false;
                 }
             }
         }
-
+        //Checks for condition (2)
         for (int i = 1; i <= 9; i++) {
             if(!_allTrue(_getCompleteRowValues(i),1)) return false;
             if(!_allTrue(_getCompleteColValues(i),1)) return false;
         }
         return true;
     }
-    //1-9
-    private boolean[] _getCompleteColValues(int absCol){
-        return _getCompleteGridElement(absCol,"col");
+
+    /**
+     * returns boolean array that represents which digits are present in the absolute column of the 9x9 grid
+     * @param absColIndex number from 1-9 that represents a column in the sudoku board
+     * @return boolean array that represents digit present in given col
+     */
+    private boolean[] _getCompleteColValues(int absColIndex){
+        return _getCompleteGridElement(absColIndex,"col");
 
     }
-    //1-9
-    private boolean[] _getCompleteRowValues(int absRow){
-        return _getCompleteGridElement(absRow,"row");
-    }
-    private boolean[]_getCompleteGridElement(int absElement,String identifier){
-        absElement = absElement -1;
-        int elementInCluster = absElement%3;
-        int clusterElementIndex = absElement/3;
+    /**
+     * returns boolean array that represents which digits are present in the row/column requested by index
+     * @param absElementIndex index of row/col requested
+     * @param identifier "row" if the index is for a row,else will return a col with the index
+     * @return boolean array that represents digit present in given row/col
+     */
+    private boolean[]_getCompleteGridElement(int absElementIndex,String identifier){
+        //Row number and the index in the array are offset by one(arrays start at 0!)
+        int elementIndex = absElementIndex -1;
+
+        // row/column of the cluster the element is in
+        int clusterElementIndex = elementIndex/3;
+
+        //the position of the element within it's cluster
+        int elementInCluster = elementIndex%3;
+
         boolean [] values = new boolean[10];
         Square3x3 currentCluster;
+
+
         for (int i = 0; i < 3; i++) {
-            if(identifier.equals("row")){
+            if (identifier.equals("row")) {
+                //Element is row
+
+                //For each cluster in the row of the cluster that the absRow is in, add the digits inside the cluster's corresponding row to a boolean array
                 currentCluster = new Square3x3(_squareCluster[clusterElementIndex][i]);
-                currentCluster.whosThereRow(elementInCluster,values);
-            }else{
+                currentCluster.whosThereRow(elementInCluster, values);
+            } else {
+                //Element is column
+
+                //For each cluster in the col of the cluster that the absCol is in, add the digits inside the cluster's corresponding col to a boolean array
                 currentCluster = new Square3x3(_squareCluster[i][clusterElementIndex]);
-                currentCluster.whosThereCol(elementInCluster,values);
+                currentCluster.whosThereCol(elementInCluster, values);
             }
         }
         return values;
     }
+
+    /**
+     * returns boolean array that represents which digits are present in the absolute row of the 9x9 grid
+     * @param absRowIndex number from 1-9 that represents a row in the sudoku board
+     * @return boolean array that represents digit present in given row
+     */
+    private boolean[] _getCompleteRowValues(int absRowIndex){
+        return _getCompleteGridElement(absRowIndex,"row");
+    }
+
+    /**
+     * Checks in a boolean array's values are all true from a specified offset
+     * @param arr boolean array to check
+     * @param offset offset to check from
+     * @return true if all values from and including the offset index in the array are true or if offset is outside of array bounds
+     */
     private boolean _allTrue(boolean arr[],int offset){
+        if(offset >= arr.length) return true; //offset is outside of array bounds, return true
+
+        //Iterator is init to array bounds
         for (int i = offset ; i < arr.length ; i++){
-            if(!arr[i]) return false;
+            if(!arr[i]) return false; // if one of the indexes in range are false return false without checking the rest
         }
         return true;
     }
